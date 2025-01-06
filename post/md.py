f0408d85110db7173e9070997bc25e806bab2b10
LIU Changle <stephane.liu.changle@gmail.com>
2025-01-06 10:19:40 +0800

    update 2025/01/06

--- post/md.py
+++ post/md.py
@@ -4,8 +4,8 @@ def main():
     adapter = LocalFileAdapter()
     converter = MdImageConverter(adapter=adapter)
     
-    md_file = MdFile(name="优化算法-动手学深度学习.zh-cn.md")
-    converter.convert(md_file, new_name="优化算法-动手学深度学习.zh-cn/优化算法-动手学深度学习.zh-cn.md")
+    md_file = MdFile(name="预备知识-动手学深度学习.md")
+    converter.convert(md_file, new_name="index.zh-cn.md")
 
 if __name__ == '__main__':
     main()
